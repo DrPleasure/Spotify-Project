81082c77c0f1078569c9e57011d05bd906aa7785
DrPleasure <114397562+DrPleasure@users.noreply.github.com>
2022-11-14 16:48:50 +0100

    updates
    updated icons:
    play button
    like (heart)
    
    - they now both change when clicked on

--- js/albumpage.js
+++ js/albumpage.js
@@ -12,3 +12,13 @@ fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=Psy", options)
   .then((response) => response.json())
   .then((response) => console.log(response))
   .catch((err) => console.error(err));
+
+// Function to toggle pause symbol onClick Play-button
+$(".fas").click(function () {
+  $(".fas").toggleClass("fa-play fa-pause");
+});
+
+// Function to toggle filled-heart symbol onClick like-button
+$(".bis").click(function () {
+  $(".bis").toggleClass("bi-heart bi-heart-fill");
+});
