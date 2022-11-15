3d4f5778ad36089c559b9f6376d34f4c6b5c3814
Nic <nic.dan.costea@gmail.com>
2022-11-15 15:01:25 +0100

    checkpoint - keyup function & reset input [done]

--- js/homepage.js
+++ js/homepage.js
@@ -1,1 +1,27 @@
-console.log("OLAF, ABI, ...");
+console.log("O, A, ...");
+
+/*-----------------------------------
+DOM elements
+-----------------------------------*/
+const search = document.getElementById("inputSearch"); // search input
+const reset = document.querySelector(".input-group-prepend"); // reset div
+
+// console the input value
+const consoleInput = (event) => {
+  let input = event.target.value;
+  console.log(input);
+};
+search.addEventListener("keyup", consoleInput);
+
+// reset the input value
+const resetInput = () => {
+  const input = document.getElementById("inputSearch");
+  if (input.value !== "") {
+    input.value = "";
+  }
+};
+reset.addEventListener("click", resetInput);
+
+/*-----------------------------------
+FETCH
+-----------------------------------*/
