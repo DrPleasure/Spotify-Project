d6262c0e7630f5a226bf307c29cba20d2124c361
abiodun <k.akinlotan@yahoo.com>
2022-11-16 12:30:35 +0000

    artist album cover fixed

--- js/artistpage.js
+++ js/artistpage.js
@@ -1,3 +1,12 @@
+const Artistpick=document.querySelector('.Artistpick')
+const artistpickplaylist=document.querySelector('.artistpickplaylist')
+const postedImg=document.querySelector('.postedImg')
+const postedby=document.querySelector('.postedby')
+const table=document.querySelector('#table')
+const currentSong=document.querySelector('.Currentsong')
+const currentsongplaylist=document.querySelector('.currentsongplaylist')
+const artistId= window.location.search
+const search= new URLSearchParams(artistId)
 const options = {
 	method: 'GET',
 	headers: {
@@ -6,16 +15,14 @@ const options = {
 	}
 };
 async function getMusic(){
- const response= await fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=queen', options);
+ try {const response= await fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=queen', options);
  const data= await response.json()
- console.log(typeof data)
- console.log(data.data)
- for(let obj of data.data){
+let artistData=data.data
+ console.log(artistData)
+ for(let obj of (artistData)){
     console.log(obj)
-    const table=document.querySelector('#table')
+let tr=document.createElement('tr')
 table.innerHTML+=`
-<tbody>
-                    <tr>
                         <th scope="row"></th>
                         <td>
                         <img src="${obj.artist.picture}" width="50px" height='50px'>
@@ -23,68 +30,44 @@ table.innerHTML+=`
                         <td>${obj.title}</td>
                         <td>1,013,238,772</td>
                         <td>${obj.duration}</td>
-                      </tr>
-                  <tr>
-                </tbody>
+                  
 `
+table.append(tr)
 }
 
+}catch (error) {
+  console.error(error)}
 }
-getMusic()
-// function renderData(song){
-// song.forEach((queenSongs) => {
-//     console.log(queenSongs)
-// });
-// }
-// window.onload=async()=>{
-// const loadData= async function(){
-// const data= await getMusic()
-// console.log(data)
-// }
-// data.forEach(obj => {
-//     console.log(obj)
-// const table=document.querySelector('#table')
-// table.innerHTML=`
-// <tbody>
-//                     <tr>
-//                         <th scope="row"></th>
-//                         <td><img src="${album.picture_big}" width="50px" height='50px'></td>
-//                         <td>${album.tittle}</td>
-//                         <td>1,013,238,772</td>
-//                         <td>3.34</td>
-//                       </tr>
-//                   <tr>
-//                     <th scope="row">2</th>
-//                     <td><img src="/assets/recentlyadded.jpeg" width="50px" height='50px'></td>
-//                     <td>Bohemian Rhapsody-Remastered 2011</td>
-//                     <td>1,013,238,772</td>
-//                     <td>5.54</td>
-//                   </tr>
-//                   <tr>
-//                     <th scope="row">3</th>
-//                     <td><img src="/assets/recentlyadded.jpeg" width="50px" height='50px'></td>
-//                     <td>Don't Stop Me Now-Remastered 2011</td>
-//                     <td>1,497,986,258</td>
-//                     <td>3.29</td>
-//                   </tr>
-//                   <tr>
-//                     <th scope="row">4</th>
-//                     <td><img src="/assets/recentlyadded.jpeg" width="50px" height='50px'></td>
-//                     <td>Under Pressure-Remastered 2011</td>
-//                     <td>900,225,096</td>
-//                     <td>4.08</td>
-//                   </tr>
-//                   <tr>
-//                     <th scope="row">5</th>
-//                     <td><img src="/assets/recentlyadded.jpeg" width="50px" height='50px'></td>
-//                     <td>We Will Rock You-Remastered 2011</td>
-//                     <td>705,225,721</td>
-//                     <td>2.02</td>
-//                   </tr>
-//                 </tbody>
-// `
 
-// });
-// })
-// }
 
+async function getAlbum(){
+  const data= await fetch('https://striveschool-api.herokuapp.com/api/deezer/artist/412',options);
+  const dataFetch= await data.json()
+  console.log(dataFetch)
+currentSong.innerHTML+=`
+
+             <img src=${dataFetch.picture_xl} width="100%" height="300px"/>
+           <div class="monthlylistners">
+                <i class="bi bi-patch-check-fill"></i>
+                Verified Artist
+                <h1>${dataFetch.name}</h1>                
+                <p>37,120,733 monthly listners</p>          
+                 </div>`         
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+window.onload= async()=>{
+ const data= await getMusic()
+ const dataFetch= await getAlbum()
+}
